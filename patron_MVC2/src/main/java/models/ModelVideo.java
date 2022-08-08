b4eec9d2a35726d12c5bdef69984d16680f5a235
Marccudi <marccudi@hotmail.es>
2022-08-08 22:49:25 +0200

    Merge branch 'main' of https://github.com/Jusfron/UD22-Patron_MVC.git into main

--- patron_MVC2/src/main/java/models/ModelVideo.java
+++ patron_MVC2/src/main/java/models/ModelVideo.java
@@ -1,5 +1,60 @@
 package models;
 
 public class ModelVideo {
+	private int id;
+	private String title;
+	private String director;
+	private int cli_id;
+	
+	public ModelVideo(int id, String title, String director, int cli_id) {
+		this.id = id;
+		this.title = title;
+		this.director = director;
+		this.cli_id = cli_id;
+	}
+	
+	public ModelVideo() {
+	}
 
+//	Getters
+	public int getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public int getCli_id() {
+		return cli_id;
+	}
+
+//	Setters
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public void setCli_id(int cli_id) {
+		this.cli_id = cli_id;
+	}
+
+	@Override
+	public String toString() {
+		return "ModelVideo [id=" + id + ", title=" + title + ", director=" + director + ", cli_id=" + cli_id + "]";
+	}
+	
+	
+	
 }
