88bf0b337a86bc97a226c863605c0c303fa159cb
David Marcos <david.13254@gmail.com>
2022-08-08 22:33:02 +0200

    viewTable

--- patron_MVC2/src/main/java/views/ViewFormVideos.java
+++ patron_MVC2/src/main/java/views/ViewFormVideos.java
@@ -17,11 +17,9 @@ import models.ModelCliente;
 
 public class ViewFormVideos extends JFrame{
 	private JPanel contentPane;
-	private JTextField txtFNombre;
-	private JTextField txtFApellido;
-	private JTextField txtFDireccion;
-	private JTextField txtFDNI;
-	private JTextField txtFFecha;
+	private JTextField txtFTitle;
+	private JTextField txtFDirector;
+	private JTextField txtFClientId;
 	private JButton btnGuardar;
 
 	public ViewFormVideos() {
@@ -36,43 +34,29 @@ public class ViewFormVideos extends JFrame{
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
-		JLabel lblNewLabel = new JLabel("Cliente");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		JLabel lblWindowTitle = new JLabel("Videos");
+		lblWindowTitle.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		
-		JLabel lblNewLabel_1_1 = new JLabel("Nombre:");
-		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		JLabel lblTitle = new JLabel("Title:");
+		lblTitle.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		
-		JLabel lblNewLabel_1_1_1 = new JLabel("Apellido:");
-		lblNewLabel_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		JLabel lblDirector = new JLabel("Director:");
+		lblDirector.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		
-		JLabel lblNewLabel_1_1_2 = new JLabel("Direccion:");
-		lblNewLabel_1_1_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		JLabel lblClientId = new JLabel("Client id:");
+		lblClientId.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		
-		JLabel lblNewLabel_1_1_3 = new JLabel("DNI:");
-		lblNewLabel_1_1_3.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		txtFTitle = new JTextField();
+		txtFTitle.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		txtFTitle.setColumns(10);
 		
-		JLabel lblNewLabel_1_1_4 = new JLabel("Fecha:");
-		lblNewLabel_1_1_4.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		txtFDirector = new JTextField();
+		txtFDirector.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		txtFDirector.setColumns(10);
 		
-		txtFNombre = new JTextField();
-		txtFNombre.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		txtFNombre.setColumns(10);
-		
-		txtFApellido = new JTextField();
-		txtFApellido.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		txtFApellido.setColumns(10);
-		
-		txtFDireccion = new JTextField();
-		txtFDireccion.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		txtFDireccion.setColumns(10);
-		
-		txtFDNI = new JTextField();
-		txtFDNI.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		txtFDNI.setColumns(10);
-		
-		txtFFecha = new JTextField();
-		txtFFecha.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		txtFFecha.setColumns(10);
+		txtFClientId = new JTextField();
+		txtFClientId.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		txtFClientId.setColumns(10);
 		
 		btnGuardar = new JButton("Guardar");
 		btnGuardar.setFont(new Font("Tahoma", Font.PLAIN, 18));
@@ -84,40 +68,27 @@ public class ViewFormVideos extends JFrame{
 			groupLayout.createParallelGroup(Alignment.TRAILING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-							.addGroup(groupLayout.createSequentialGroup()
-								.addComponent(lblNewLabel)
-								.addContainerGap(638, Short.MAX_VALUE))
-							.addGroup(groupLayout.createSequentialGroup()
-								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-									.addGroup(groupLayout.createSequentialGroup()
-										.addComponent(lblNewLabel_1_1_1, GroupLayout.DEFAULT_SIZE, 64, Short.MAX_VALUE)
-										.addGap(69)
-										.addComponent(txtFApellido, GroupLayout.DEFAULT_SIZE, 178, Short.MAX_VALUE))
-									.addGroup(groupLayout.createSequentialGroup()
-										.addComponent(lblNewLabel_1_1, GroupLayout.DEFAULT_SIZE, 66, Short.MAX_VALUE)
-										.addGap(67)
-										.addComponent(txtFNombre, GroupLayout.DEFAULT_SIZE, 178, Short.MAX_VALUE)))
-								.addGap(380))
-							.addGroup(groupLayout.createSequentialGroup()
-								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-										.addGroup(groupLayout.createSequentialGroup()
-											.addComponent(lblNewLabel_1_1_2, GroupLayout.DEFAULT_SIZE, 129, Short.MAX_VALUE)
-											.addPreferredGap(ComponentPlacement.RELATED))
-										.addGroup(groupLayout.createSequentialGroup()
-											.addComponent(lblNewLabel_1_1_3, GroupLayout.DEFAULT_SIZE, 41, Short.MAX_VALUE)
-											.addGap(92)))
-									.addGroup(groupLayout.createSequentialGroup()
-										.addComponent(lblNewLabel_1_1_4)
-										.addGap(92)))
-								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-									.addComponent(txtFFecha, GroupLayout.DEFAULT_SIZE, 178, Short.MAX_VALUE)
-									.addComponent(txtFDNI, 178, 178, Short.MAX_VALUE)
-									.addComponent(txtFDireccion, Alignment.TRAILING, 178, 178, Short.MAX_VALUE))
-								.addGap(380)))
-						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblWindowTitle)
+							.addContainerGap(638, Short.MAX_VALUE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(lblDirector, GroupLayout.DEFAULT_SIZE, 65, Short.MAX_VALUE)
+									.addGap(69)
+									.addComponent(txtFDirector, GroupLayout.DEFAULT_SIZE, 179, Short.MAX_VALUE))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(lblTitle, GroupLayout.DEFAULT_SIZE, 67, Short.MAX_VALUE)
+									.addGap(67)
+									.addComponent(txtFTitle, GroupLayout.DEFAULT_SIZE, 179, Short.MAX_VALUE)))
+							.addGap(380))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblClientId, GroupLayout.DEFAULT_SIZE, 130, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(txtFClientId, 178, 179, Short.MAX_VALUE)
+							.addGap(380))
+						.addGroup(groupLayout.createSequentialGroup()
 							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 								.addComponent(lblError)
 								.addComponent(btnGuardar, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE))
@@ -127,42 +98,32 @@ public class ViewFormVideos extends JFrame{
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(lblNewLabel)
+					.addComponent(lblWindowTitle)
 					.addGap(18)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(txtFNombre, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_1_1, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtFTitle, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblTitle, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE))
 					.addGap(18)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel_1_1_1, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
-						.addComponent(txtFApellido, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE))
+						.addComponent(lblDirector, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtFDirector, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE))
 					.addGap(18)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel_1_1_2, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
-						.addComponent(txtFDireccion, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE))
-					.addGap(18)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel_1_1_3, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
-						.addComponent(txtFDNI, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE))
-					.addGap(18)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel_1_1_4, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
-						.addComponent(txtFFecha, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE))
-					.addGap(59)
+						.addComponent(lblClientId, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtFClientId, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE))
+					.addGap(141)
 					.addComponent(btnGuardar, GroupLayout.PREFERRED_SIZE, 39, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(lblError)
-					.addContainerGap(34, Short.MAX_VALUE))
+					.addContainerGap(51, Short.MAX_VALUE))
 		);
 		getContentPane().setLayout(groupLayout);
 	}
 	
-	public void form(ModelCliente cliente) {
-		txtFNombre.setText(cliente.getNombre());
-		txtFApellido.setText(cliente.getApellido());
-		txtFDireccion.setText(cliente.getDireccion());
-		txtFDNI.setText(String.valueOf(cliente.getDni()));
-		txtFFecha.setText(cliente.getDate());
+	public void form(ModelVideos video) {
+		txtFTitle.setText(video.getTitle());
+		txtFDirector.setText(video.getDirector());
+		txtFClientId.setText(video.getClientId());
 	}
 	
 	
@@ -171,22 +132,14 @@ public class ViewFormVideos extends JFrame{
 	}
 
 	public String getTxtFNombre() {
-		return txtFNombre.getText();
+		return txtFTitle.getText();
 	}
 
 	public String getTxtFApellido() {
-		return txtFApellido.getText();
+		return txtFDirector.getText();
 	}
 
 	public String getTxtFDireccion() {
-		return txtFDireccion.getText();
-	}
-
-	public String getTxtFDNI() {
-		return txtFDNI.getText();
-	}
-
-	public String getTxtFFecha() {
-		return txtFFecha.getText();
+		return txtFClientId.getText();
 	}
 }
--- patron_MVC2/src/main/java/views/ViewTable.java
+++ patron_MVC2/src/main/java/views/ViewTable.java
@@ -23,6 +23,7 @@ public class ViewTable extends JFrame {
 	private JButton btnEditar;
 	private JButton btnBorrar;
 	private JButton btnCrear;
+	private JButton btnCambiar;
 	
 	/**
 	 * Create the frame.
@@ -48,6 +49,9 @@ public class ViewTable extends JFrame {
 		btnCrear.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		
 		JScrollPane scrollPane = new JScrollPane();
+		
+		btnCambiar = new JButton("Cambiar Tabla");
+		btnCambiar.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
@@ -56,17 +60,19 @@ public class ViewTable extends JFrame {
 					.addComponent(lblCliente, GroupLayout.PREFERRED_SIZE, 107, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap(586, Short.MAX_VALUE))
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(63)
-					.addComponent(btnEditar, GroupLayout.PREFERRED_SIZE, 98, GroupLayout.PREFERRED_SIZE)
-					.addGap(142)
-					.addComponent(btnBorrar, GroupLayout.PREFERRED_SIZE, 98, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 765, Short.MAX_VALUE)
-					.addComponent(btnCrear, GroupLayout.PREFERRED_SIZE, 98, GroupLayout.PREFERRED_SIZE)
-					.addGap(76))
-				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGap(30)
-					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 651, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(22, Short.MAX_VALUE))
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+							.addComponent(btnCambiar)
+							.addGap(58)
+							.addComponent(btnEditar, GroupLayout.PREFERRED_SIZE, 98, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 96, Short.MAX_VALUE)
+							.addComponent(btnBorrar, GroupLayout.PREFERRED_SIZE, 98, GroupLayout.PREFERRED_SIZE)
+							.addGap(82)
+							.addComponent(btnCrear, GroupLayout.PREFERRED_SIZE, 98, GroupLayout.PREFERRED_SIZE)
+							.addGap(20))
+						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 651, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap())
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
@@ -75,11 +81,12 @@ public class ViewTable extends JFrame {
 					.addComponent(lblCliente, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)
 					.addGap(7)
 					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 282, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 150, Short.MAX_VALUE)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+					.addPreferredGap(ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnCambiar, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnCrear, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
 						.addComponent(btnEditar, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnBorrar, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnCrear, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE))
+						.addComponent(btnBorrar, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE))
 					.addGap(30))
 		);
 		
