1556274fc199ec7abaac3918d9935e71544d8ac8
David Marcos <david.13254@gmail.com>
2022-08-08 09:47:58 +0200

    Controller

--- patron_MVC/src/main/java/controllers/Controller.java
+++ patron_MVC/src/main/java/controllers/Controller.java
@@ -1,5 +1,15 @@
 package controllers;
 
+import models.ModelCliente;
+import views.ViewCliente;
+
 public class Controller {
+	
+	ModelCliente modelCliente;
+	ViewCliente viewCliente;
+	
+	public Controller (ModelCliente modelCliente, ViewCliente viewCliente) {
+		
+	}
 
 }
